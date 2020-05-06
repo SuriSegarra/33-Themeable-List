c925e63853ee2a0c4b543f7e28a798a62e752752
surelis segarra <sdarianne34@gmail.com>
2020-05-06 16:01:43 -0700

    created api fetch

---
+++ src/services/apifetch.js
@@ -0,0 +1,11 @@
+export const fetchCharacter = () => {
+  return fetch('https://rickandmortyapi.com/api/character')
+    .then(res => res.json())
+    .then(res => ({
+      image: res.image,
+      name: res.name,
+      species: res.species,
+      status: res.status,
+      origin: res.origin
+    }));
+};
